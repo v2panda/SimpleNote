8cb8437b0e66f8597db18553ebd3e795a8df1604
徐臻 <xuzhen@panda.local>
2015-03-17 09:20:48 +0800

    SimpleNote First Commit

---
+++ SimpleNote/SimpleNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleNote
+//
+//  Created by 徐臻 on 15/3/17.
+//  Copyright (c) 2015年 xuzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleNote/SimpleNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleNote
+//
+//  Created by 徐臻 on 15/3/17.
+//  Copyright (c) 2015年 xuzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
