589178bd67b87b6c32e2fda0afcc0b5ab22e39c1
panda <302751643@qq.com>
2015-03-19 11:01:40 +0800

    Update VNNote.h

--- SimpleNote/SimpleNote/VNNote.h
+++ SimpleNote/SimpleNote/VNNote.h
@@ -7,7 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+#define VNNOTE_DEFAULT_TITLE @"无标题笔记"
+@interface VNNote : NSObject<NSCoding>
 
-@interface VNNote : NSObject
+@property (nonatomic, strong) NSString *noteID;
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *content;
+@property (nonatomic, strong) NSDate *createdDate;
+@property (nonatomic, strong) NSDate *updatedDate;
+@property (nonatomic, assign) NSInteger index;
+@property (nonatomic, strong) NSString *encryptStr;
+
+- (id)initWithTitle:(NSString *)title
+            content:(NSString *)content
+        createdDate:(NSDate *)createdDate
+         updateDate:(NSDate *)updatedDate encrypt:(NSString *)encryptStr;
+
+- (BOOL)Persistence;
 
 @end
