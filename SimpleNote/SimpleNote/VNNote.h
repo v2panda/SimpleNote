90b30f1f03bdb576bbfa8975cb557602a23632c0
徐臻 <xuzhen@panda.local>
2015-03-19 10:56:45 +0800

    003

---
+++ SimpleNote/SimpleNote/VNNote.h
@@ -0,0 +1,13 @@
+//
+//  VNNote.h
+//  SimpleNote
+//
+//  Created by 徐臻 on 15/3/19.
+//  Copyright (c) 2015年 xuzhen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VNNote : NSObject
+
+@end
---
+++ SimpleNote/SimpleNote/VNNote.m
@@ -0,0 +1,13 @@
+//
+//  VNNote.m
+//  SimpleNote
+//
+//  Created by 徐臻 on 15/3/19.
+//  Copyright (c) 2015年 xuzhen. All rights reserved.
+//
+
+#import "VNNote.h"
+
+@implementation VNNote
+
+@end
