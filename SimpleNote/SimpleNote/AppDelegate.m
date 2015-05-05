65d3c2836c323e5d8c94577c7efe1f25c683d36f
xuzhen2014 <302751643@qq.com>
2015-05-05 14:32:05 +0800

    Security

--- SimpleNote/SimpleNote/AppDelegate.m
+++ SimpleNote/SimpleNote/AppDelegate.m
@@ -10,6 +10,7 @@
 #import "VNConstants.h"
 #import "NoteManager.h"
 #import "VNNote.h"
+#import "SecurityStrategy.h"
 
 @interface AppDelegate ()
 {
@@ -50,6 +51,9 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    //添加模糊效果
+    [SecurityStrategy addBlurEffect];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -63,6 +67,9 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    //移除模糊效果
+    [SecurityStrategy removeBlurEffect];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
