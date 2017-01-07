de2685ed14a0acc9a25d30696098e1ab265c48a1
v2panda <pdxuzhen@gmail.com>
2017-01-07 13:42:02 +0800

    add findpw

--- SimpleNote/SimpleNote/Sections/Controllers/Login&Register/ForgetPWViewController.m
+++ SimpleNote/SimpleNote/Sections/Controllers/Login&Register/ForgetPWViewController.m
@@ -8,7 +8,10 @@
 
 #import "ForgetPWViewController.h"
 
-@interface ForgetPWViewController ()
+@interface ForgetPWViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *emailTextfield;
+@property (weak, nonatomic) IBOutlet UIButton *findButton;
 
 @end
 
@@ -16,7 +19,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
+
+#pragma mark - event response
+- (IBAction)findButtonDidTouched:(UIButton *)sender {
+    if (!self.emailTextfield.text.length) {
+        kTipAlert(@"请先输入邮箱");
+        return;
+    }
+    if (![self isValidEmail:self.emailTextfield.text]) {
+        self.emailTextfield.text = @"";
+        kTipAlert(@"请输入正确的邮箱地址");
+        return;
+    }
+    [AVUser requestPasswordResetForEmailInBackground:self.emailTextfield.text block:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            kTipAlert(@"已向该邮箱发送重置邮件，注意查收！");
+        } else {
+            NSLog(@"errorcode : %ld",error.code);
+            if (error.code == 205) {
+                kTipAlert(@"该用户尚未注册");
+            }
+        }
+    }];
+}
+
+- (BOOL)isValidEmail:(NSString *)email {
+    NSString *regex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTestPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [emailTestPredicate evaluateWithObject:email];
+}
+
+#pragma mark - UITextFieldDelegate
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    if ([self isValidEmail:textField.text]) {
+        textField.text = @"";
+        kTipAlert(@"请输入正确的邮箱地址");
+    }
+}
+
 
 @end
